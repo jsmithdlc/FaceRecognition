2a294f31d8a429d2e1a93c334e73f2aa7ebe5f75
Javier <javier.smith.dlc@gmail.com>
2021-03-15 19:31:02 -0300

    recognizes people in img and draws over them

--- src/recognize.py
+++ src/recognize.py
@@ -15,29 +15,56 @@ use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
 torch.backends.cudnn.benchmark = True
 
+embeddings_model = InceptionModelV1()
+embeddings_model.load_weights("../models/20180402-114759-vggface2.pt")
+embeddings_model.eval()
+anchor_embeddings = pd.read_csv("../database/embeddings/embeddings.csv",index_col=0)
+
+yolo_model = Darknet("../models/yolo_config.cfg")
+yolo_model.load_weights("../models/yolov3-wider_16000.pt")
+yolo_model.eval()
+yolo_input_size = (416,416)
+pil2torch = transforms.ToTensor()
+
+def detect_people(img_path):
+    img = Image.open(img_path)
+    size = img.size
+    x_scale = size[0]/yolo_input_size[0]
+    y_scale = size[1]/yolo_input_size[1]
+    net_img = img.resize(yolo_input_size)
+
+    detections = yolo_model(pil2torch(net_img).unsqueeze(dim=0))
+    detections = non_max_suppression(detections)[0].detach().numpy()
+    return detections, (x_scale, y_scale)
+
+"""
+def post_process(image, scales, detections):
+    x_scale, y_scale = scales
+    for box in detections:
+        bbox = box.detach().numpy()
+"""
+
+
+
+
+def show_detections(img, scale, detections):
+    x_scale, y_scale = scale
+    draw = ImageDraw.Draw(img)
+    for bbox in detections:
+        x1, x2 = bbox[0] * x_scale, bbox[2] * x_scale
+        y1, y2 = bbox[1] * y_scale, bbox[3] * y_scale
+        draw.rectangle([x1,y1,x2,y2], width = 4)
+    img.save("../out_imgs/detection", "JPEG")
+
+
+
 
 
 if __name__ == '__main__':
-    embeddings_model = InceptionModelV1()
-    embeddings_model.load_weights("../models/20180402-114759-vggface2.pt")
-    embeddings_model.eval()
-    anchor_embeddings = pd.read_csv("../database/embeddings/embeddings.csv",index_col=0)
-
-    yolo_model = Darknet("../models/yolo_config.cfg")
-    yolo_model.load_weights("../models/yolov3-wider_16000.pt")
-    yolo_model.eval()
-
-    original_img = Image.open('/home/javier/Ramblings/FaceRecognition/sample_imgs/lore_boat.jpg')
-    original_size = original_img.size
-    net_size = (416,416)
-    x_scale = original_size[0]/416
-    y_scale = original_size[1]/416
-
-    net_img = original_img.resize(net_size)
-
-
-    out_det = yolo_model(transforms.ToTensor()(net_img).unsqueeze(dim=0))
-    out_det = non_max_suppression(out_det)
+    img_path = '/home/javier/Ramblings/FaceRecognition/sample_imgs/cabros.jpg'
+    detections, scale = detect_people(img_path)
+    show_detections(Image.open(img_path), scale, detections)
+    """
     bbox = out_det[0].detach().numpy()[0]
     bbox[0], bbox[2] = bbox[0] * x_scale, bbox[2] * x_scale
     bbox[1], bbox[3] = bbox[1] * y_scale, bbox[3] * y_scale
@@ -45,6 +72,7 @@ if __name__ == '__main__':
     draw = ImageDraw.Draw(original_img)
     draw.rectangle(bbox[:4], width = 4)
     original_img.save("../out_imgs/lore_detected","JPEG")
+    """
 
 
 
